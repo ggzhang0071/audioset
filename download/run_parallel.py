4ad7c685b5c3820706faf7f41e6e704662d22f8b
Victor Li <vili@nvidia.com>
2022-05-22 22:45:20 +0800

    tmp

--- download/run_parallel.py
+++ download/run_parallel.py
@@ -5,15 +5,20 @@ import os
 
 
 def download_oneline(line_input):
-    os.system("./download.sh {}".format(line_input))
+    cmd="./download.sh {}".format(line_input)
+    print(cmd)
+    os.system(cmd)
 
 if __name__=="__main__":
     #csv_list=["eval_segments.csv","unbalanced_train_segments.csv"]
     csv_list=["test.csv"]
     for csv_file in csv_list:
         wav_info_list=[]
-        with open(csv_file,"r") as f:   
+        with open(os.path.join("/mnt/work/git/audioset/download",csv_file),"r") as f:   
             for line in f.readlines():
                 wav_info_list.append(line.strip())
         with Pool(cpu_count()) as p:
             p.map(download_oneline,wav_info_list)
+
+
+#os.system("./download.sh -7TanrCbmME, 30.000, 40.000, /m/04rlf,/m/09x0r,/t/dd00005")
