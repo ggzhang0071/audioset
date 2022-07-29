6006539ff3a956c9b5d96bc79f1103f502ac341f
ggzhang0071 <155051843@qq.com>
2022-07-29 18:21:14 +0800

    tmp

---
+++ download/requirement.sh
@@ -0,0 +1,9 @@
+
+conda config --add channels conda-forge
+conda install -y ffmpeg
+conda install -y numpy
+conda install -y  pandas
+python3.8 -m pip install youtube-dl
+python3.8 -m pip install pandas  
+
+
